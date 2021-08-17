390dfbdba06b7f5598ec46d47f55b0a455ba142a
Breno Hildebrand <breno.h1ldebr4nd@gmail.com>
2021-08-17 07:01:13 -0700

    Dia 1 - Exercício 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,25 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const app = document.querySelector('[data-js="app"]');
+const button = document.querySelector('button');
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+button.addEventListener('click', (event) => {
+  toggleAppVisibility();
+}, false);
+
+const toggleAppVisibility = () => {
+  const isAppVisible = app.style.display !== 'none';
+  isAppVisible ? hideApp() : showApp();
+}
+
+const hideApp = () => app.style.display = 'none';
+
+const showApp = () => app.style.display = 'block';
+
+
+
+
