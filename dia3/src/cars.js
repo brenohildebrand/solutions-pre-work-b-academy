67c413ae227c2f81264669013fce9d1fda6efacc
Breno Hildebrand <breno.h1ldebr4nd@gmail.com>
2021-08-20 10:33:13 -0700

    Dia 3 - Exercícios refatorados

--- dia3/src/cars.js
+++ dia3/src/cars.js
@@ -7,40 +7,21 @@ const plateInput = document.querySelector('[data-js=placa]')
 const colorInput = document.querySelector('[data-js=cor]')
 const table = document.querySelector('[data-js=table]')
 
-const currentYear = new Date().getFullYear();
-const firstYearConsidered = 1980;
-
-const years = new Array(currentYear-firstYearConsidered+1);
-
-for(let i = 0; i < years.length; i++)
-  years[i] = firstYearConsidered + i;
-
-yearInput.innerHTML = years.reduce((options, year) => {
-  return options + `<option value="${year}">${year}</option>`
-}, '')
-
 form.addEventListener('submit', (event) => {
   event.preventDefault()
 
-  const url = event.target.elements.url.value;
-  const marca = event.target.elements.marca.value;
-  const modelo = event.target.elements.modelo.value;
-  const ano = event.target.elements.ano.value;
-  const placa = event.target.elements.placa.value;
-  const cor = event.target.elements.cor.value;
+  const { url, marca, modelo, ano, placa, cor } = event.target.elements
 
   const row = document.createElement('tr')
 
-  row.innerHTML = `
-      <td>${url}</td>
-      <td>${marca}</td>
-      <td>${modelo}</td>
-      <td>${ano}</td>
-      <td>${placa}</td>
-      <td>${cor}</td>
-  `
+  row.appendChild(createImage(url.value))
+  row.appendChild(createRowElement(marca.value))
+  row.appendChild(createRowElement(modelo.value))
+  row.appendChild(createRowElement(ano.value))
+  row.appendChild(createRowElement(placa.value))
+  row.appendChild(createColor(cor.value))
 
-  table.appendChild(row)
+  table.children[1].appendChild(row)
 
   urlInput.value = ''
   brandInput.value = ''
@@ -50,7 +31,40 @@ form.addEventListener('submit', (event) => {
   colorInput.value = ''
 
   urlInput.focus()
-
 })
+
+const createImage = (url) => {
+  const td = createRowElement('')
+
+  const img = document.createElement('img')
+  img.setAttribute('src', url)
+  img.style.maxWidth = '192px'
+  img.style.maxHeight = '108px'
+
+  td.appendChild(img)
+
+  return td
+}
+
+const createRowElement = (text) => {
+  const td = document.createElement('td')
+  td.innerText = text
+
+  return td
+}
+
+const createColor = (color) => {
+  const td = createRowElement('')
+
+  const div = document.createElement('div')
+  div.style.display = 'inline-block'
+  div.style.width = '80px'
+  div.style.height = '80px'
+  div.style.backgroundColor = color
+
+  td.appendChild(div)
+
+  return td
+}
 
 
--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,107 +1,88 @@
 // Exercício 01
-
 const nameInput = document.querySelector("[data-js=name]")
+const dontCapitalize = ['de', 'da', 'das', 'do', 'dos']
 
 nameInput.addEventListener('input', (event) => {
-  event.target.value = formatInput(event.target.value)
+  const name = event.target.value;
+  event.target.value = formatText(name);
 })
 
-const formatInput = (string) => {
-  const lower = string.toLowerCase()
+const formatText = (text) => {
+  const lower = text.toLowerCase()
   const words = lower.split(' ')
 
-  const capitalizedWords = words.map((word) => {
-    switch(word) {
-      case 'de':
-      case 'da':
-      case 'do':
-      case 'dos':
-        break
-      default:
-        word = capitalize(word)
-    }
+  const formattedText = words
+  .map((word) => dontCapitalize.includes(word) ? word: capitalize(word))
+  .join(' ')
 
-    return word
-  })
-
-  return capitalizedWords.join(' ')
+  return formattedText
 }
 
 const capitalize = (word) => {
-
-  const firstLetter = word.charAt(0)
-  const lower = word.toLowerCase()
-
-  return firstLetter.toUpperCase() + lower.slice(1)
-
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 }
 
 // Exercício 02
+const colorsForm = document.querySelector('[data-js=color-form]')
+const colorsLabel = document.createElement('label')
+colorsLabel.setAttribute('value', 'colors')
+colorsLabel.innerText = 'Colors'
 
-function createDiv (color) {
+const createColor = (color) => {
   const div = document.createElement('div')
 
   div.style.width = '100px'
   div.style.height = '100px'
   div.style.backgroundColor = color
-
-  div.classList.add('hidden')
+  div.style.display = 'none'
 
   return div
 }
 
-function createDivs (...colors) {
-  const divs = {};
+const createColorsMap = (...colors) => {
+  const colorsMap = {};
 
-  for(let i = 0; i < colors.length; i++) {
-    const currentColor = colors[i]
-    divs[currentColor] = createDiv(currentColor)
-  }
+  colors.map((currentColor) => colorsMap[currentColor] = createColor(currentColor))
 
-  return divs
+  return colorsMap
 }
 
-const form = document.querySelector('form')
-
-// Label
-const colorsLabel = document.createElement('label')
-colorsLabel.setAttribute('value', 'colors')
-colorsLabel.innerText = 'Colors'
-
-
-form.appendChild(colorsLabel)
-
-// Select and Divs
-const organizer = document.createElement('div');
-organizer.classList.add('organizer')
-
-const colorBoxes = createDivs('red', 'green', 'blue', 'black', 'yellow')
+const colorsMap = createColorsMap('red', 'green', 'blue', 'black', 'yellow')
+const colorsOrganizer = document.createElement('div');
+Object.values(colorsMap).forEach((div) => colorsOrganizer.appendChild(div))
+colorsOrganizer.style.display = 'flex'
+colorsOrganizer.style.flexDirection = 'row'
+colorsOrganizer.style.justifyContent = 'center'
+colorsOrganizer.style.margin = 'auto'
+colorsOrganizer.style.height = '100px'
 
 const colorsSelect = document.createElement('select')
 colorsSelect.setAttribute('multiple', '')
-colorsSelect.innerHTML = Object.keys(colorBoxes).reduce((options, color) => {
-  return options + `<option value="${color}">${color}</value>`
-}, '')
+Object.keys(colorsMap).map((color) => {
+  const option = document.createElement('option')
+  option.value = color
+  option.innerText = color
 
-form.appendChild(colorsSelect)
-form.insertAdjacentElement('afterend', organizer);
-Object.values(colorBoxes).forEach((div) => organizer.append(div))
+  colorsSelect.appendChild(option)
+})
+
+colorsForm.appendChild(colorsLabel)
+colorsForm.appendChild(colorsSelect)
+colorsForm.insertAdjacentElement('afterend', colorsOrganizer);
 
 colorsSelect.addEventListener('change', (event) => {
 
   const options = event.target.options
 
-  for(let i = 0; i < options.length; i++) {
+  for(let i = 0; i < options.length; i++){
 
     const currentOption = options[i]
-    const currentColor = options[i].value
+    const currentColor = currentOption.value
 
     if(currentOption.selected === true){
-      colorBoxes[currentColor].classList.remove('hidden')
+      colorsMap[currentColor].style.display = 'block'
     } else {
-      colorBoxes[currentColor].classList.add('hidden')
+      colorsMap[currentColor].style.display = 'none'
     }
-
   }
-
 })
